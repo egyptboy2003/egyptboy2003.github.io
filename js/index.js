e5c93a8ab26bbb5bddafeaff17d9e3daca4982b0
egyptboy2003 <snake.d2003@gmail.com>
2020-10-15 12:22:30 +1300

    added more fields

--- js/index.js
+++ js/index.js
@@ -12,7 +12,9 @@ function getUserData() {
 function writeUserData(fname, lname, pwd, email) {
   database.ref("venturers/" + fname).set({
     firstname: fname,
-    password: pwd
+    lastname: lname,
+    password: pwd,
+    email: email
   });
 }
 
