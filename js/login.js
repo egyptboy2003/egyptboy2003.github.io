df1f44ddbdcb2932d5acbc9e6ef2568d349e9a80
jakethesnake003 <58544487+jakethesnake003@users.noreply.github.com>
2020-11-09 20:17:45 +1300

    added functionality to login/register buttons
    login not yet complete

--- js/login.js
+++ js/login.js
@@ -1,23 +1,44 @@
 var database = firebase.database();
 
-function getUserData() {
+// Register user data
+function registerUser() {
     var form = document.getElementById('registerform');
-    var fname = form.fnameinput.value;
-    var lname = form.lnameinput.value;
-    var pwd = form.pwdinput.value;
-    var email = form.emailinput.value;
-    alert(fname + lname + pwd + email);
-    writeUserData(fname, lname, pwd, email);
-}
-
-function writeUserData(fname, lname, pwd, email) {
-    database.ref('venturers/' + fname).set({
-        firstname: fname,
-        lastname: lname,
-        password: pwd,
-        email: email
+    database.ref('venturers').push().set({
+        firstname: form.fnameinput.value,
+        lastname: form.lnameinput.value,
+        password: form.pwdinput.value,
+        email: form.emailinput.value
     });
 }
+// Login
+function isValidUser() {
+    var form = document.getElementById('registerform');
+    var email = form.emailinput.value;
+    var pwd = form.pwdinput.value;
+    database.ref('venturers').once('value', function(snapshot) {
+        goal = snapshot.val();
+        sleepGoalInput.value = goal;
+    });
 
-document.getElementById('registerbtn').addEventListener('click', getUserData);
-document.getElementById('loginbtn').addEventListener('click', getUserData);
+}
+
+// Changes sign in boxes
+function toLogin() {
+    document.getElementById('registerform').style.display = 'none';
+    document.getElementById('loginform').style.display = 'inline';
+    document.getElementById('registerbtn').style.backgroundColor = '#333';
+    document.getElementById('loginbtn').style.backgroundColor = '#222';
+}
+
+function toRegister() {
+    document.getElementById('loginform').style.display = 'none';
+    document.getElementById('registerform').style.display = 'inline';
+    document.getElementById('registerbtn').style.backgroundColor = '#222';
+    document.getElementById('loginbtn').style.backgroundColor = '#333';
+}
+
+// Add event listeners for the login buttons & submit buttons
+document.getElementById('registerbtn').addEventListener('click', toRegister);
+document.getElementById('loginbtn').addEventListener('click', toLogin);
+document.getElementById('registersubmit').addEventListener('click', registerUser);
+document.getElementById('loginsubmit').addEventListener('click', toLogin);
