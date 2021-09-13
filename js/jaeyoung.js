321a4ab64b458194f42da38cd529068909bcd6fb
egyptboy2003 <30887987+egyptboy2003@users.noreply.github.com>
2021-09-14 09:53:55 +1200

    need to center text, multi line working

--- js/jaeyoung.js
+++ js/jaeyoung.js
@@ -1,19 +1,41 @@
-const poss_options = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+const poss_options = [
+    "Mentions Otago or scholarships",
+    "'Are you alright Georgia'",
+    "Warn Asher about his calculator",
+    "Harrasses someone (has to mention Korea)",
+    "Argues with Jane",
+    "Jumps into someone else's conversation",
+    "Threatens Someone",
+    "Argues with Patrick/Heshan/Aaron",
+    "Getting 5+ textbooks for friends",
+    "Whips head around",
+    "Fidget Spinner",
+    "Deep sigh",
+    "Stay out of it / None of your business",
+    "Groans",
+    "Fans self intensely"
+]
 var used_nums = []
 const squares = document.getElementsByClassName('square');
 var num = 0;
 
 function changeColor() {
-    this.style.backgroundColor = "blue";
+    if (this.style.backgroundColor == "red") {
+        this.style.backgroundColor = "#50f";
+    } else {
+        this.style.backgroundColor = "red"
+    }
 }
 
 function setupGrid() {
     Array.from(squares).forEach(element => {
-        do {
-            num = Math.floor(Math.random() * 9);
-        } while (used_nums.includes(num));
-        used_nums.push(num);
-        element.innerHTML = poss_options[num];
+        if (element.id != "5") {
+            do {
+                num = Math.floor(Math.random() * 14);
+            } while (used_nums.includes(num));
+            used_nums.push(num);
+            element.children[0].innerHTML = poss_options[num];
+        }
     });
 }
 
